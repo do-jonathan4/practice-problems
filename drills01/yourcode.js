df1ab75eee7ae913322b594ddeb6d74989e5ad58
do-jonathan4 <d.johnt45@gmail.com>
2022-12-28 00:26:41 -0800

    practice sumArray

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,6 +1,10 @@
 
-function sumArray(){
-	
+function sumArray(arr){
+    let sum = 0
+	for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum
 }
 
 function fitWithinVal(){
