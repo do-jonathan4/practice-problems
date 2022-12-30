bec889e63475dd1acbd3ba4d6f3b4100953dbbb8
do-jonathan4 <d.johnt45@gmail.com>
2022-12-29 18:25:49 -0800

    attempted fitwithval

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -7,8 +7,16 @@ function sumArray(arr){
     return sum
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(arr, val){
+    let sum = 0
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > val) continue
+        if (sum >= val) continue
+        sum += arr[i]
+        result.push(arr[i])
+    }
+    return result
 }
 
 function getAllNamesShorterThan(){
