a7581657e57a13ed6052b3e7583bd614a82658c1
do-jonathan4 <d.johnt45@gmail.com>
2023-01-02 06:36:55 -0800

    practiced fitwithval and getallnamesshorterthan

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -11,16 +11,19 @@ function fitWithinVal(arr, val){
     let sum = 0
     let result = []
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > val) continue
-        if (sum >= val) continue
+        if (sum + arr[i] > val) continue
         sum += arr[i]
         result.push(arr[i])
     }
     return result
 }
 
-function getAllNamesShorterThan(){
-
+function getAllNamesShorterThan(names, num){
+    let result = []
+    for (let i = 0; i < names.length; i++) {
+        if (names[i].length < num) result.push(names[i])
+    }
+    return result
 }
 
 function makeLabel(){
