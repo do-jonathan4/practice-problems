8f2f8b59166129065f58fcffcf8f64e84f084bae
do-jonathan4 <d.johnt45@gmail.com>
2023-01-03 07:14:35 -0800

    completed countOccurences

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,7 +1,11 @@
 
 
-function countOccurences(){
-
+function countOccurences(arr, word){
+    let result = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === word) result++
+    }
+    return result
 }
 
 function wordLengths(){
