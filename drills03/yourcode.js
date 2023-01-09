a3b75f3b1d4daf79540cd6deef2a073092565096
do-jonathan4 <d.johnt45@gmail.com>
2023-01-09 06:17:56 -0800

    completed correctCalcChecker

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -17,7 +17,22 @@ function getCapitalCount(arr){
 }
 
 function correctCalcChecker(arr){
-
+    let correct = []
+    for (let obj of arr) {
+        if (obj.op === '+') {
+            if (obj['result'] === obj.num1 + obj.num2) correct.push(obj)
+        }
+        if (obj.op === '-') {
+            if (obj['result'] === obj.num1 - obj.num2) correct.push(obj)
+        }
+        if (obj.op === '*') {
+            if (obj['result'] === obj.num1 * obj.num2) correct.push(obj)
+        }
+        if (obj.op === '/') {
+            if (obj['result'] === obj.num1 / obj.num2) correct.push(obj)
+        }
+    }
+    return correct
 }
 
 function doMath(){
