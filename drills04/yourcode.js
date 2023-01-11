90b182ee8c13ab46a334324e4d2f9f62d07564a4
do-jonathan4 <d.johnt45@gmail.com>
2023-01-11 09:46:02 -0800

    completed changeElements

--- drills04/yourcode.js
+++ drills04/yourcode.js
@@ -1,7 +1,18 @@
 
 
-function changeElements(){
-
+function changeElements(numClass){
+    let numConvert = document.querySelectorAll(numClass)
+    numConvert.forEach(x => {
+        if (x.textContent === 'one') x.textContent = 1
+        if (x.textContent === 'two') x.textContent = 2
+        if (x.textContent === 'three') x.textContent = 3
+        if (x.textContent === 'four') x.textContent = 4
+        if (x.textContent === 'five') x.textContent = 5
+        if (x.textContent === 'six') x.textContent = 6
+        if (x.textContent === 'seven') x.textContent = 7
+        if (x.textContent === 'eight') x.textContent = 8
+        if (x.textContent === 'nine') x.textContent = 9
+    })
 }
 
 function appendTextToElement(){
