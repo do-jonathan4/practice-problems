6c0915c58027a0156db0bb79d779eb8e7a0f97a2
do-jonathan4 <d.johnt45@gmail.com>
2023-01-17 15:26:51 -0800

    attempted populateRecords

--- drills05/yourcode.js
+++ drills05/yourcode.js
@@ -1,7 +1,29 @@
 
 
-function populateRecords(){
+function populateRecords(arr){
+    const tbl = document.querySelector('#testArea table')
+    let row = document.createElement("tr")
+    
+    let obj = {
+        "charge": 0,
+        "cash advance": 0
+    }
 
+    arr.forEach(x => {
+        for (const prop in x) {
+            let cell = document.createElement("td")
+            cell.textContent = x[prop]
+            row.append(cell)
+        }
+        tbl.append(row)
+    })
+    console.log(tbl)
+
+    arr.forEach(x => {
+        if (x.type === "charge") obj["charge"] += Number(x.amount)
+        if (x.type === "cash advance") obj["cash advance"] += Number(x.amount)
+    });
+    return obj
 }
 
 
