bd9e5612dfea1a0cf437b7e5e95554f71672ee70
do-jonathan4 <d.johnt45@gmail.com>
2023-01-17 16:00:20 -0800

    made table

--- drills05/yourcode.js
+++ drills05/yourcode.js
@@ -1,15 +1,25 @@
 
 
 function populateRecords(arr){
-    const tbl = document.querySelector('#testArea table')
+    let testArea = document.querySelector('#testArea')
+    var tbl = document.createElement("table")
     let row = document.createElement("tr")
-    
+    row.insertAdjacentHTML("afterbegin", "<th>TYPE</th>")
+    row.insertAdjacentHTML("beforeend", "<th>SOURCE</th>")
+    row.insertAdjacentHTML("beforeend", "<th>AMOUNT</th>")
+    row.classList.add('transactionRecord')
+    tbl.append(row)
+
+    row.childNodes[0].classList.add("transactionType")
+    row.childNodes[1].classList.add("transactionLocation")
+    row.childNodes[2].classList.add("transactionAmount")
     let obj = {
         "charge": 0,
         "cash advance": 0
     }
 
     arr.forEach(x => {
+        let row = document.createElement("tr") 
         for (const prop in x) {
             let cell = document.createElement("td")
             cell.textContent = x[prop]
@@ -17,7 +27,7 @@ function populateRecords(arr){
         }
         tbl.append(row)
     })
-    console.log(tbl)
+    testArea.append(tbl)
 
     arr.forEach(x => {
         if (x.type === "charge") obj["charge"] += Number(x.amount)
